c9b40e1348264b133a58204c4e6525c0cea13804
Haydar Kulekci <haydarkulekci@gmail.com>
2024-11-20 16:13:46 +0300

    collpase search result on search

--- src/Search.php
+++ src/Search.php
@@ -175,6 +175,14 @@ class Search
     private $scroll;
 
     /**
+     * You can use the collapse parameter to collapse search results based on field values. The
+     * collapsing is done by selecting only the top sorted document per collapse key.
+     *
+     * @var array
+     */
+    private $collapse;
+
+    /**
      * @var OrderedSerializer
      */
     private static $serializer;
@@ -769,6 +777,13 @@ class Search
         return $this;
     }
 
+    public function setCollapse(array $collapse)
+    {
+        $this->collapse = $collapse;
+
+        return $this;
+    }
+
     /**
      * @param string $name
      * @param string|array|bool $value
@@ -843,6 +858,7 @@ class Search
             'minScore' => 'min_score',
             'searchAfter' => 'search_after',
             'trackTotalHits' => 'track_total_hits',
+            'collapse' => 'collapse'
         ];
 
         foreach ($params as $field => $param) {
